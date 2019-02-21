cb4e3bc549583cfb56b253285ff38ea43a8d7576
Whitny Edwards <wne4e2@gmail.com>
2019-02-21 00:18:57 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,1 +1,5 @@
-class add_column < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents< ActiveRecord::Migration[5.1]
+  def change
+    add_column :grade, :birthdate, string
+  end 
+end 

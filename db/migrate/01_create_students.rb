b581f9d464fe015b2cd9b89c407a4a9c46cc7cd3
Whitny Edwards <wne4e2@gmail.com>
2019-02-21 00:15:35 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change 
-    create_table :   do |t|
-      t.
+    create_table :students   do |t|
+      t.string 
       t.
     end 
   end 
